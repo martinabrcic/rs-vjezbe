3f28b6d0e371cab210df594646f13f7961ac0029
martinabrcic <brcicmartina5@gmail.com>
2024-11-07 20:56:21 +0100

    vjezba 6

---
+++ vjezba_6_analiza_for_petlji.py
@@ -0,0 +1,12 @@
+#prva - nema rangea, range je 1
+
+for i in range(1, 2):
+  print(i)
+
+# druga - ispisat će brojeve 1, 3, 5, 7 i 9
+for i in range(1, 10, 2):
+  print(i)
+
+#treća - ispisat će brojeve od 10 do 2
+for i in range(10, 1, -1):
+  print(i)
