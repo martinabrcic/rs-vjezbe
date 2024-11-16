6d22e89d526c032cc9b59af46933d0194415b18c
martinabrcic <brcicmartina5@gmail.com>
2024-11-16 23:05:18 +0100

    zadatak 2 lambde kvadriranje sa zbrajanjem

---
+++ vjezbe_2/zad_2.py
@@ -0,0 +1,8 @@
+#zbroji pa kvadriraj
+
+def zbroji_pa_kvadriraj(a, b):
+    return (a + b) ** 2
+print(zbroji_pa_kvadriraj(4, 5))
+
+kvadriranje_sa_zbrajanjem = lambda x, y: (x+y) ** 2
+print(kvadriranje_sa_zbrajanjem(3, 2))
