b8add0588d5b922c4e8652817ec7ac01c2248c73
martinabrcic <brcicmartina5@gmail.com>
2024-12-14 18:55:47 +0100

    zad 3, punoljetni

--- vjezbe_5/zad_1.py
+++ vjezbe_5/zad_1.py
@@ -1,9 +1,10 @@
 #Definirajte aiohttp poslužitelj koji radi na portu 8081 koji na putanji /proizvodi vraća listu proizvoda u JSON formatu. Svaki proizvod je rječnik koji sadrži ključeve naziv, cijena i količina. Pošaljite zahtjev na adresu http://localhost:8080/proizvodi koristeći neki od HTTP klijenata ili curl i provjerite odgovor.
 from aiohttp import web
 
-async def get_proizvodi(request):
-    proizvodi = [{'naziv': 'tipkovnica', 'cijena': 50, 'kolicina': 3},
+proizvodi = [{'naziv': 'tipkovnica', 'cijena': 50, 'kolicina': 3},
                  {'naziv': 'miš', 'cijena': 30, 'kolicina': 5}]
+
+async def get_proizvodi(request):
     return web.json_response(proizvodi)
 app = web.Application()
 app.router.add_get('/proizvodi', get_proizvodi)
